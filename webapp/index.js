91b916a7654e649e16e05c2c15022d485b3debd7
chandaranakeyur <chandaranakeyur@gmail.com>
2019-07-08 16:09:20 +0000

    first push
    Change-Id: I1e4918810d95713a2e21986c8d29af4906012c53

---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/m/Text"
+
+], function (ABC , DEF) {
+	"use strict";
+
+	new ABC({
+		text: "keyur World"
+		
+	}).placeAt("name");
+	
+	new DEF({
+		text: "aaa World"
+		
+	}).placeAt("name");
+});
